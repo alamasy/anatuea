66eec05307707764fb1ac5ee7e92e476a090bcfe
alamasy <alamwidiasyarie@gmail.com>
2024-02-28 22:33:43 +0700

    Akhirnya kembali seperti semula 2

--- app/task-manager/page.js
+++ app/task-manager/page.js
@@ -1,52 +1,74 @@
-"use client"
+"use client";
 
 import { useState } from "react";
-import "./task.css"
+import "./task.css";
 import Title from "../components/Title";
 
-export default function TaskManager () {
-    const [ isDragged, setIsDragged ] = useState(false)
-    const isDragging = "is-dragging"
+export default function TaskManager() {
+	const [isDragged, setIsDragged] = useState(false);
+	const isDragging = "is-dragging";
 
-    const handleDrag = () => {
-        console.log("dragged");
-        setIsDragged(true)
-    }
+	const handleDrag = () => {
+		console.log("dragged");
+		setIsDragged(true);
+	};
 
-    const handleDragOver = (e) => {
-        e.preventDefault()
-        alert("hello")
-    }
+	const handleDragOver = (e) => {
+		e.preventDefault();
+		alert("hello");
+	};
 
-    return (
-        <div className="board">
-            <Title>Task Manager</Title>
-            <div className="todo-form">
-                <input type="text" placeholder="New Todo" />
-                <button type="submit">Add</button>
-            </div>
+	return (
+		<div className="board">
+			<Title>Task Manager</Title>
+			<div className="todo-form">
+				<input type="text" placeholder="New Todo" />
+				<button type="submit">Add</button>
+			</div>
 
-            <div className="lanes">
-                <div className="swim-lane">
-                    <h2>TODO</h2>
+			<div className="lanes">
+				<div className="swim-lane">
+					<h2>TODO</h2>
 
-                    <p className={`task ${isDragged ? isDragging : ""}`} draggable="true" onDragStart={handleDrag} onDragEnd={() => setIsDragged(false)}>Jemput Siti</p>
-                    <p className="task" draggable="true">Beberes rumah</p>
-                    <p className="task" draggable="true">Belajar</p>
-                </div>
+					<p
+						className={`task ${
+							isDragged
+								? isDragging
+								: ""
+						}`}
+						draggable="true"
+						onDragStart={handleDrag}
+						onDragEnd={() =>
+							setIsDragged(false)
+						}>
+						Jemput
+					</p>
+					<p className="task" draggable="true">
+						Beberes rumah
+					</p>
+					<p className="task" draggable="true">
+						Belajar
+					</p>
+				</div>
 
-                <div className="swim-lane" onDragOver={handleDragOver}>
-                    <h2>DOING</h2>
+				<div
+					className="swim-lane"
+					onDragOver={handleDragOver}>
+					<h2>DOING</h2>
 
-                    <p className="task" draggable="true">Belajar</p>
-                </div>
+					<p className="task" draggable="true">
+						Belajar
+					</p>
+				</div>
 
-                <div className="swim-lane">
-                    <h2>DONE</h2>
+				<div className="swim-lane">
+					<h2>DONE</h2>
 
-                    <p className="task" draggable="true">Beli sari kacang hijau</p>
-                </div>
-            </div>
-        </div>
-    )
+					<p className="task" draggable="true">
+						Beli sari kacang hijau
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
