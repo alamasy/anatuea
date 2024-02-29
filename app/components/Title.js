bfbbc9cc326237b45852a996b3add4c3a95ff926
alamasy <alamwidiasyarie@gmail.com>
2024-02-29 10:29:09 +0700

    Styling title component with tailwind for fast developing

--- app/components/Title.js
+++ app/components/Title.js
@@ -1,9 +1,9 @@
-import { title } from "./title.module.css"
-
-export default function Title ({ children }) {
-    return (
-        <div>
-            <h1 className={title}>{children}</h1>
-        </div>
-    )
+export default function Title({ children }) {
+	return (
+		<div>
+			<h1 className="text-5xl font-bold text-center mb-8">
+				{children}
+			</h1>
+		</div>
+	);
 }
