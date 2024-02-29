23c1071aa70a305be4f1bef3f61ea1f0911fcd86
alamasy <alamwidiasyarie@gmail.com>
2024-02-29 09:41:12 +0700

    install and configure tailwind css and daisyui

--- app/page.js
+++ app/page.js
@@ -4,17 +4,6 @@ export default function Home() {
 	return (
 		<>
 			<Title>Home Page</Title>
-			<p>aku</p>
-			<p>adalah</p>
-			<p>anak</p>
-			<p>gembala</p>
-			<p>selalu</p>
-			<p>riang</p>
-			<p>serta</p>
-			<p>gembira</p>
-			<p>karena</p>
-			<p>aku</p>
-			<p>rajin</p>
 		</>
 	);
 }
